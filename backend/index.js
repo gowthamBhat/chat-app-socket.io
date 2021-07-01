f67aa5f8533294806e79386681f41da42277d55c
Gowtham  H D <gowthambhat793@gmail.com>
2021-07-02 00:44:32 +0530

    refactoring the Port config

--- backend/index.js
+++ backend/index.js
@@ -3,15 +3,19 @@ const app = express();
 
 const server = require('http').createServer(app);
 //go through socket.io docs
-const io = require('socket.io')(server); //creating an Io server
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*'
+  }
+}); //creating socket
 
 io.on('connection', (socket) => {
-  console.log('What is Socket', socket);
+  // console.log('What is Socket', socket);
   console.log('Socket is active to be connected!!!');
   socket.on('chat', (payload) => {
-    console.log('What is Payload', payload);
+    // console.log('What is Payload', payload);
     io.emit('chat', payload);
   });
 });
-const PORT = 5500;
-server.listen(PORT, () => console.log(`Server is Listing on Port ${PORT}... `));
+const PORT = process.env.PORT || 5500;
+server.listen(PORT, () => console.log(`Listing on Port ${PORT}... `));
