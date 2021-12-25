881da4cd7c2213e71abaa0eae8a242188f867905
Gowtham  H D <gowthambhat793@gmail.com>
2021-12-26 00:56:28 +0530

    project merging to foodeis app

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
-const server = require('http').createServer(app);
+const server = require('http').createServer(app)
 //go through socket.io docs
 const io = require('socket.io')(server, {
   cors: {
     origin: '*'
   }
-}); //creating socket
+}) //creating socket
 
 io.on('connection', (socket) => {
   // console.log('What is Socket', socket);
-  console.log('Socket is active to be connected!!!');
+  console.log('Socket is active to be connected!!!')
   socket.on('chat', (payload) => {
     // console.log('What is Payload', payload);
-    io.emit('chat', payload);
-  });
-});
-const PORT = process.env.PORT || 5500;
-server.listen(PORT, () => console.log(`Listing on Port ${PORT}... `));
+    io.emit('chat', payload)
+  })
+})
+const PORT = process.env.PORT || 5500
+server.listen(PORT, () => console.log(`Listing on Port ${PORT}... `))
